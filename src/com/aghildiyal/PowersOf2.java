75edc4b3945eaa3857199741b87d464fea21fa13
ankghild <aghildiyal@sapient.com>
2020-05-10 13:42:55 -0400

    First commit

---
+++ src/com/aghildiyal/PowersOf2.java
@@ -0,0 +1,25 @@
+package com.aghildiyal;
+
+public class PowersOf2 {
+
+    public static void main(String[] d){
+
+        int n = 50;
+        powerOf2(50);
+    }
+
+    public static int powerOf2(int n){
+        if(n < 0){
+            return 0;
+        }if(n == 1){
+            System.out.println(1);
+            return 1;
+        }else{
+            int prev = powerOf2(n/2);
+            int current = prev*2;
+            System.out.println(current);
+            return current;
+        }
+    }
+
+}
